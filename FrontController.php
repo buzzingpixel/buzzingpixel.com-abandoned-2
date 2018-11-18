c49d793f0314b313e48438adf5c250f8d7d79720
TJ Draper <tj@buzzingpixel.com>
2018-11-17 20:35:00 -0600

    Load DotEnv in front controller

--- FrontController.php
+++ FrontController.php
@@ -9,4 +9,8 @@ define('APP_VENDOR_PATH', APP_BASE_PATH . $sep . 'vendor');
 
 require_once APP_VENDOR_PATH . $sep . 'autoload.php';
 
+if (file_exists(APP_BASE_PATH . $sep . '.env')) {
+    (new Dotenv\Dotenv(APP_BASE_PATH))->load();
+}
+
 echo 'hello front controller world!';
