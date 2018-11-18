6b8728bbed6ceceefaa30939e916d8c047e96264
TJ Draper <tj@buzzingpixel.com>
2018-11-18 16:18:39 -0600

    Define Atlas action queue relationships

--- src/app/data/ActionQueue/ActionQueueRelationships.php
+++ src/app/data/ActionQueue/ActionQueueRelationships.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 namespace src\app\data\ActionQueue;
 
 use Atlas\Mapper\MapperRelationships;
+use src\app\data\ActionQueueItem\ActionQueueItem;
 
 class ActionQueueRelationships extends MapperRelationships
 {
-    protected function define()
+    protected function define(): void
     {
+        $this->oneToMany('action_queue_items', ActionQueueItem::class, [
+            'guid' => 'action_queue_guid',
+        ]);
     }
 }
--- src/app/data/ActionQueueItem/ActionQueueItemRelationships.php
+++ src/app/data/ActionQueueItem/ActionQueueItemRelationships.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 namespace src\app\data\ActionQueueItem;
 
 use Atlas\Mapper\MapperRelationships;
+use src\app\data\ActionQueue\ActionQueue;
 
 class ActionQueueItemRelationships extends MapperRelationships
 {
-    protected function define()
+    protected function define(): void
     {
+        $this->manyToOne('action_queue', ActionQueue::class, [
+            'action_queue_guid' => 'guid',
+        ]);
     }
 }
